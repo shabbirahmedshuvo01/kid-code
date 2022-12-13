f613413dac4d498443385e2fc517b822b3f853f9
unknown <sabbirshuvo006@gmail.com>
2022-12-13 15:01:44 +0600

    text edited

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
         <div>
             <div className='flex mx-28 h-96 mt-20'>
                 <div className='text-start'>
-                    <h1 className='text-4xl font-semibold'>CODING BOOTCAMP FOR KIDS</h1>
+                    <h1 className='text-4xl font-semibold'>Online Creation FOR KIDS</h1>
                     <p className='text-xl'>Brought to you by India’s First Coding Bootcamp</p>
                     <p className='text-xl text-primary'>The Hacking School</p>
 
